2946f5625b640a0238058cd5c828281a470df23d
Ilyas <irtimirbulatov@gmail.com>
2017-01-22 21:45:05 +0300

    решил поменять логику, создал классы Оператора и Клиента, на перспективу

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import telebot
 import util
 
 bot = telebot.TeleBot(config.token)
-client_operator_chat = {}
 
 
 # Реагирует на /start, /help, просто отдаёт приветственное сообщение
@@ -14,83 +13,33 @@ def send_welcome(message):
 
 
 # Авторизация операторов по паролю
-@bot.message_handler(regexp='\/login \d*$')
+@bot.message_handler(regexp='\/login .*$')
 def login_operator(message):
-    if util.message_num(message.text) == 123:
-        f = open('operator.txt', 'r')
-        for line in f:
-            if str(message.chat.id) in line:
-                bot.send_message(message.chat.id, 'Вы уже вошли как оператор')
-                f.close()
-                break
-        else:
-            f = open('operator.txt', 'a')
-            f.write(str(message.chat.id) + '\n')
-            f.close()
-            bot.send_message(message.chat.id, 'Вы вошли как оператор')
-    else:
-        bot.send_message(message.chat.id, 'ты что-то не то ввёл')
+    util.Operator(message.chat.id).auth_operator(message.text)
 
 
 # Деаутентификация операторов
 @bot.message_handler(commands=['logoff'])
 def logoff_operator(message):
-    try:
-        del (client_operator_chat[message.chat.id])
-    except KeyError:
-        pass
-    util.ReplaceLineInFile('operator.txt', str(message.chat.id) + '\n', '')
-    bot.reply_to(message, 'Вы больше не оператор')
+    util.Operator(message.chat.id).deauth_operator()
 
 
 # Рассылает  всем операторам сообщения от клиента
 @bot.message_handler(func=lambda message: util.client_operator(message.chat.id))
 def chat_with_operator(message):
-    global client_operator_chat
-    for chat_id in util.file_in_list():
-        bot.send_message(chat_id, 'Клиент с номером чата /chat_{client_chat_id} +  написал:\n{msg_text}'.format(
-            client_chat_id=str(message.chat.id), msg_text=message.text))
-        # Исключение для того чтобы сохранялся последний выбранный чат, а для новых операторов добавлялось значение None
-        try:
-            if client_operator_chat[int(chat_id)] is not None:
-                continue
-        except KeyError:
-            client_operator_chat[int(chat_id)] = None  # добавляет всех новых операторов в словарь и значение None
+    util.Client(message.chat.id).message_client_operator(message.text)
 
 
 # Позволяет оператору выбрать чат с каким клиентом общаться
 @bot.message_handler(regexp='\/chat_\d*$')
 def set_chat_operator_to_client(message):
-    try:
-        client_operator_chat[message.chat.id] = util.message_num(message.text)
-    except ValueError:
-        pass
+    util.Operator(message.chat.id).set_client(message.text)
 
 
 # Смотрит с кем выбрал общаться оператор и направляет сообщения от оператора - клиенту и всем оставшимся операторам
 @bot.message_handler(func=lambda message: not util.client_operator(message.chat.id))
 def chat_with_client(message):
-    try:
-        if client_operator_chat[message.chat.id] is None:
-            bot.send_message(message.chat.id,
-                             'для ответа нужному клиенту, необходимо нажать над его сообщением на /chat_...')
-        else:
-            bot.send_message(int(client_operator_chat[message.chat.id]), message.text)
-            for chat_id in util.file_in_list():
-                if message.chat.id == int(chat_id):
-                    continue
-                else:
-                    try:
-                        bot.send_message(chat_id,
-                                         'Оператор {operator_chat_id} ответил клиенту '
-                                         '{client_chat_id}:\n{msg_text}'.format(
-                                             operator_chat_id=str(message.chat.id),
-                                             client_chat_id=str(client_operator_chat[message.chat.id]),
-                                             msg_text=message.text))
-                    except KeyError:
-                        continue
-    except KeyError:
-        bot.send_message(message.chat.id, 'клиент ещё ничего не писал')
+    util.Operator(message.chat.id).message_operator_client(message.text)
 
 
 # Запуск бота, стараемся не обращать внимания на ошибки
--- util.py
+++ util.py
@@ -1,6 +1,92 @@
 # -*- coding: utf-8 -*-
 import re
 
+from bot import bot
+
+client_operator_chat = {}
+
+
+class Operator:
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+    # Функция аутентификации операторов по паролю
+    def auth_operator(self, message_text):
+        # Здесь можно установить пароль для входа операторов (default: 123)
+        if message_pass(message_text) == '123':
+            f = open('operator.txt', 'r')
+            for line in f:
+                if str(self.chat_id) in line:
+                    bot.send_message(self.chat_id, 'Вы уже вошли как оператор')
+                    f.close()
+                    break
+            else:
+                f = open('operator.txt', 'a')
+                f.write(str(self.chat_id) + '\n')
+                f.close()
+                bot.send_message(self.chat_id, 'Вы вошли как оператор')
+        else:
+            bot.send_message(self.chat_id, 'ты что-то не то ввёл')
+
+    # Функция деаутентификации операторов
+    def deauth_operator(self):
+        try:
+            del (client_operator_chat[self.chat_id])
+        except KeyError:
+            pass
+        replace_line_in_file('operator.txt', str(self.chat_id) + '\n', '')
+        bot.send_message(self.chat_id, 'Вы больше не оператор')
+
+    # Функция выбора оператором чата с клиентом
+    def set_client(self, message_text):
+        try:
+            client_operator_chat[self.chat_id] = message_num(message_text)
+        except ValueError:
+            pass
+
+            # Функция опеределяет с кем выбрал общаться оператор и направляет сообщения от оператора - клиенту и всем
+            # остальным операторам
+
+    def message_operator_client(self, message_text):
+        try:
+            if client_operator_chat[self.chat_id] is None:
+                bot.send_message(self.chat_id,
+                                 'для ответа нужному клиенту, необходимо нажать над его сообщением на /chat_...')
+            else:
+                bot.send_message(int(client_operator_chat[self.chat_id]), message_text)
+                for chat_id in file_in_list():
+                    if self.chat_id == int(chat_id):
+                        continue
+                    else:
+                        try:
+                            bot.send_message(chat_id,
+                                             'Оператор {operator_chat_id} ответил клиенту '
+                                             '{client_chat_id}:\n{msg_text}'.format(
+                                                 operator_chat_id=str(self.chat_id),
+                                                 client_chat_id=str(client_operator_chat[self.chat_id]),
+                                                 msg_text=message_text))
+                        except KeyError:
+                            continue
+        except KeyError:
+            bot.send_message(self.chat_id, 'клиент ещё ничего не писал')
+
+
+class Client:
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
+    # Функция рассылает всем операторам сообщения от клиента
+    def message_client_operator(self, message_text):
+        global client_operator_chat
+        for chat_id in file_in_list():
+            bot.send_message(chat_id, 'Клиент с номером чата /chat_{client_chat_id} +  написал:\n{msg_text}'.format(
+                client_chat_id=str(self.chat_id), msg_text=message_text))
+            try:
+                if client_operator_chat[int(chat_id)] is not None:
+                    continue
+            except KeyError:
+                client_operator_chat[int(chat_id)] = None  # добавляет всех новых операторов в словарь и значение None
+
 
 def file_in_list():
     f = open('operator.txt', 'r')
@@ -16,15 +102,20 @@ def client_operator(chat_id):
         return True
 
 
+def message_pass(msg):
+    result = re.search(r'^/login\s(.*)$', msg).group(1)
+    return result
+
+
 def message_num(msg):
-    number = int((re.search(r'\d*$', msg)).group(0))
-    return number
+    result = re.search(r'(\d*)$', msg).group(0)
+    return result
 
 
-def ReplaceLineInFile(fileName, sourceText, replaceText):
-    file = open(fileName, 'r')
+def replace_line_in_file(file_name, seurce_text, replace_text):
+    file = open(file_name, 'r')
     text = file.read()
     file.close()
-    file = open(fileName, 'w')
-    file.write(text.replace(sourceText, replaceText))
+    file = open(file_name, 'w')
+    file.write(text.replace(seurce_text, replace_text))
     file.close()
